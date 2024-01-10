f7e4e9a384a545fbe127c52c13dcbcc1959f15c5
Exequiel <alvarado.exequiel@gmail.com>
2024-01-10 20:31:47 -0300

    acceso
    acceso a login

--- registerApp/src/app/app-routing.module.ts
+++ registerApp/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'listar-qr',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
--- registerApp/src/app/login/login.page.ts
+++ registerApp/src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  usuario = new FormGroup({
+    user: new FormControl('',[Validators.required, Validators.minLength(4),Validators.maxLength(20)]),
+    pass: new FormControl('',[Validators.required, Validators.minLength(4),Validators.maxLength(20)]),
+  });
 
+  docente = "aaaa";
+  pass1 ="aaaa"
+
+  login(){
+    console.log(this.usuario.value.user);
+    if(this.usuario.value.user==this.docente){
+      console.log("ingresamos bien"),
+      this.router.navigate(['/home'])
+      
+    }
+  };
+  
+  constructor(private router: Router){}
+  navigate(){
+   
+  }
   ngOnInit() {
   }
 
+  
+  
+ 
 }
