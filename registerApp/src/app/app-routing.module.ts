10d5c5dc0b1eaa6f30b8e8252b0165c688d27e38
Exequiel <alvarado.exequiel@gmail.com>
2024-01-18 12:59:33 -0300

    add page 404
    se agrega pagina 404

--- registerApp/src/app/app-routing.module.ts
+++ registerApp/src/app/app-routing.module.ts
@@ -27,6 +27,10 @@ const routes: Routes = [
     path: 'inicio',
     loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
   },
+  {
+    path: 'page404',
+    loadChildren: () => import('./page404/page404.module').then( m => m.Page404PageModule)
+  },
 ];
 
 @NgModule({
